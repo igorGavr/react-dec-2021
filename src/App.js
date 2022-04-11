996f350d9b4a17da0d4f82c5a7223a588a71e293
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-04-11 23:02:59 +0300

    lesson3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {useState} from "react";
+
+import {CarForm, Cars} from "./components";
+
 const App = () => {
+    const [newCar, setNewCar] = useState(null);
+    const [carForUpdate,setCarForUpdate] = useState(null);
+
     return (
         <div>
-            App
+            <CarForm setNewCar={setNewCar} carForUpdate={carForUpdate}/>
+            <hr/>
+            <Cars newCar={newCar} setCarForUpdate={setCarForUpdate}/>
         </div>
     );
 };
