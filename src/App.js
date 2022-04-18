f9458933cf7618a2d2303802d481f2fdffb1278e
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-04-18 21:38:41 +0300

    lesson5_part2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,37 @@
+import {useReducer} from "react";
+
+const init = (initCount) => {
+    return {count1: initCount, count2: initCount}
+}
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'inc':
+            return {...state, count1: state.count1 + 1}
+        case 'dec':
+            return {...state, count1: state.count1 - 1}
+        case 'reset':
+            return {...state, count1: 0}
+        case 'set':
+            return {...state, count1: action.payload}
+    }
+}
+
 const App = () => {
+    const [state, dispatch] = useReducer(reducer, 0, init);
+
+    // const inc = () => {
+    //   dispatch({type:'inc'})
+    // }
     return (
         <div>
-            App
+            <div>
+                {state.count1}
+                <button onClick={() => dispatch({type: 'inc'})}>inc</button>
+                <button onClick={() => dispatch({type: 'dec'})}>dec</button>
+                <button onClick={() => dispatch({type: 'reset'})}>reset</button>
+                <button onClick={() => dispatch({type: 'set', payload: 10})}>setTo10</button>
+            </div>
         </div>
     );
 };
