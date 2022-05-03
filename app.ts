e2b183a3a610abd48bf8f614f399476b3ba70af9
Vitaliy Demchyshyn <demchyshyn.v87@gmail.com>
2022-05-03 20:38:11 +0300

    typescript hw resolve

--- app.ts
+++ app.ts
@@ -1,137 +1,108 @@
-// // const show = (a: number, b: number): number | string => {
-// //     return {}
-// // }
-//
-//
-// // const user = {name: 'Max', age: 15}
-//
-//
-// // const showUser = (user: { name: string, age: number }): void => {
-// //     user.
-// // }
-// // const user: { name: string, age: number } = {age: 45, name: 'Max'}
-// //
-// // console.log(user);
-//
-//
-// // const show = (a: number, b: number): { name: string, age: number}  => {
-// //     return {}
-// // }
-// type UniqId = number
-//
-// interface IUser<Type, G> { // T by default
-//     id?: UniqId,
-//     name: string,
-//     age: number
-//     // address: IAddress
-//     arr: Type[]
-//     arr2: G[]
+// 1) создать интерфейс на основе этого объекта:
+
+interface ICore {
+    flight: number,
+    core: {
+        reuse_count: number,
+        status: string
+    }
+}
+
+interface IPayload {
+    payload_type: string,
+    payload_mass_kg: number,
+    payload_mass_lbs: number
+}
+
+interface ISpaceX {
+    mission_name: string
+    launch_date_local: string
+    launch_site: {
+        site_name_long: string
+    },
+    links: {
+        article_link: string,
+        video_link: string
+    },
+    rocket: {
+        rocket_name: string
+        first_stage: {
+            cores: ICore[]
+        },
+        second_stage: {
+            payloads: IPayload[]
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////
+// 2) протипизировать функции:
+// interface IUser {
+//     name: string;
+//     age: number;
+//     gender: string
 // }
 //
-// interface IAddress {
-//     street: string,
-//     house: number
+// const user: IUser = {
+//     name: "Max",
+//     age: 18,
+//     gender: 'male'
 // }
 //
+// const sum = (a: number, b: number): number => a + b;
 //
-// // const user: Partial<IUser> = {}
-// // const user: IUser<string, number> = {name: 'Max', age: 15, arr: ['1', '2', '3'], arr2: [2]}
-// //
-// // console.log(user);
-// //
-// // type StateType = [string, (a: number, b: number) => number]
-// //
-// // const useState: StateType = ['text', (a, b) => {
-// //     return a + b
-// // }]
-// //
-// // let [first, second] = useState;
-// //
-// // console.log(first);
-// // console.log(second(3, 4));
-// //
-// // interface IMyArray{
-// //     arr:[number, string, IUser<number, string>]
-// // }
-// //
-// // const my:IMyArray = {arr:[4,'ee',{name:'dddd', age:1, arr:[1,2,3], arr2:['s']}, 333]}
+// const showSum = (a: number, b: number): void => {
+//     console.log(a + b);
+// };
 //
+// const incAge = (someUser: IUser, inc: number): IUser => {
+//     someUser.age += inc
+//     return someUser
+// };
 //
-// // class User {
-// //     constructor(private readonly id: number, protected name: string, public age: number) {
-// //         this.id = id;
-// //         this.name = name;
-// //         this.age = age;
-// //     }
-// //
-// //     getId(): number {
-// //         return this.id
-// //     }
-// //
-// //     setId(newId: number): void {
-// //         this.id = newId
-// //     }
-// // }
-// //
-// // const user: User = {name: 'Max', age: 5}
-// //
-// //
-// // let user1 = new User(13, 'Kira', 15);
-// // user1.getId()
-// // user1.
+// console.log(sum(1, 2));
+// showSum(2, 3)
+// incAge(user, 2)
 //
-//
-// interface IShapeActions {
-//     area: () => number;
-//     perimeter: () => number;
-// }
-//
-// interface Issss{
-//     hhh:(a:string, b:string)=>void;
-// }
-//
-// class Triangle implements IShapeActions, Issss {
-//     constructor(public a: number, private b: number, private c: number) {
-//     }
-//
-//     hhh(a: string, b: string){
-//         console.log(a+b)
-//     }
-//
-//     area(): number {
-//         return this.a * this.b * this.c
-//     }
-//
-//     perimeter(): number {
-//         return this.a + this.b + this.c
-//     }
-//
-//
-// }
-//
-// class Rectangle implements IShapeActions {
-//     constructor(private a: number, private b: number) {
-//     }
-//
-//     area(): number {
-//         return this.a * this.b
-//     }
-//
-//     perimeter(): number {
-//         return this.a + this.b
-//     }
-//
-//
-// }
-//
-// const shapes: IShapeActions[] = [new Triangle(1, 2, 3), new Rectangle(2, 7), new Triangle(1, 5, 8)]
-//
-// for (let shape of shapes) {
-//     console.log(shape.area());
-//     console.log(shape.perimeter());
-// }
-// let rectangle = new Rectangle(2, 7);
-// rectangle.
+
+////////////////////////////////////////////////////
+
+type Name = 'Max' | 'Kira' | 'Karina' | 'Olha'
+
+interface IUser {
+    id?: number
+    name: Name
+}
+
+interface Iuser2 extends IUser {
+    age: number
+}
+
+const user: Required<Iuser2> = {name: 'Kira', id: null, age: 23}
+
+const str = 'sdfsdf'
+
+console.log(typeof str);
 
 
-console.log('dddddddddddddddddddddd');
+class User{
+    constructor(public name:string, public age:number) {
+    }
+    getName():string{
+        return this.name
+    }
+}
+
+interface functional {
+    getAge:()=>number
+}
+class User2 extends User implements functional{
+
+    constructor(name: string, age: number) {
+        super(name, age);
+    }
+
+    getAge(): number {
+        return this.age;
+    }
+}
--- enums/my.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum MyEnum {
-    ADD = 'ADD',
-    DELETE = 'DELETE',
-    UPDATE = 'UPDATE'
-}
-
